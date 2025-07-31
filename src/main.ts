2b66c4b27e86ac6bd7da7829923e1104479a3f7e
wahyu.suit <wahyu.suit@anteraja.id>
2025-07-31 07:39:06 +0700

    add an currency Rp.

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,10 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { registerLocaleData } from '@angular/common';
+import localeId from '@angular/common/locales/id';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+registerLocaleData(localeId, 'id-ID');
+bootstrapApplication(AppComponent, appConfig).catch((err) =>
+  console.error(err)
+);
